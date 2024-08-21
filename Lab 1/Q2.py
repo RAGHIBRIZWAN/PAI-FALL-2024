1182aa9bff79fa456c4d4040655ae8a73bb26c38
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-08-21 19:50:35 +0500

    Update Q2.py

--- Lab 1/Q2.py
+++ Lab 1/Q2.py
@@ -2,8 +2,8 @@ input1 = int(input('Enter 1st value: '))
 op = input('Enter the operator: ')
 input2 = int(input('Enter 2nd value: '))
 
-if op == '+':
-    result = input1 + input2
+if op == '--':
+    result = input1 - (-input2)
     
 elif op == '-':
     result = input1 - input2
