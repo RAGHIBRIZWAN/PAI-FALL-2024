4a0013bfe23f2b81f5f06f20f8fa7fa1e96e72fa
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-08-21 08:28:58 +0500

    Create Q2.py

---
+++ Lab 1/Q2.py
@@ -0,0 +1,17 @@
+input1 = int(input('Enter 1st value: '))
+op = input('Enter the operator: ')
+input2 = int(input('Enter 2nd value: '))
+
+if op == '+':
+    result = input1 + input2
+    
+elif op == '-':
+    result = input1 - input2
+    
+elif op == '*':
+    result = input1 * input2
+    
+elif op == '/':
+    result = input1 / input2
+    
+print(result)
