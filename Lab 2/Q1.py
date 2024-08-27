ec4e0e9668f8c61b26bcd6c6623859332cfdef91
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-08-27 21:17:30 +0500

    Create Q1.py

---
+++ Lab 2/Q1.py
@@ -0,0 +1,8 @@
+def calculate_area(length,width):
+    area = length * width
+    return area
+
+length = int(input('Enter the length: '))
+width= int(input('Enter the width: '))
+
+print(calculate_area(length,width))
