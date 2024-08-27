384e8843d4239d12e9ad693a5c388f63dc430a80
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-08-27 21:23:27 +0500

    Update Q2.py

--- Lab 2/Q2.py
+++ Lab 2/Q2.py
@@ -2,8 +2,8 @@ def check(word):
     check = word[len(word) - 1]
     for i in range(len(word)):
         if check == 'a' or check == 'e' or check == 'i' or check == 'o' or check == 'u':
-            return 'Last word is vowel!'
-    return 'Last word is constant!'
+            return 'Last letter is vowel!'
+    return 'Last letter is consonant!'
 
 word = str(input('Enter the word: '))
 
