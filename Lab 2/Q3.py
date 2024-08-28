4ec48f4e18bd83abb6d6f4e960d187ce1a475fdd
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-08-28 09:33:42 +0500

    Update Q3.py

--- Lab 2/Q3.py
+++ Lab 2/Q3.py
@@ -1,1 +1,13 @@
+def url_check(url):
+    check = ''
+    ans = ''
+    for i in range(11):
+        check += url[i]
 
+    if check == 'http://www.':
+        for i in range(11,len(url)):
+            ans += url[i]
+
+    return ans
+
+print(url_check('http://www.raghib.com'))
