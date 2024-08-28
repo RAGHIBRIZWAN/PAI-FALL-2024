79c972ba0a708c466fcadf46598bd8d8447d8865
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-08-28 10:00:36 +0500

    Update Q3.py

--- Lab 2/Q3.py
+++ Lab 2/Q3.py
@@ -1,3 +1,5 @@
+#METHOD 1:
+
 def url_check(url):
     check = ''
     ans = ''
@@ -9,5 +11,16 @@ def url_check(url):
             ans += url[i]
 
     return ans
+
+print(url_check('http://www.raghib.com'))
+
+#METHOD 2:
+
+def url_check(url):
+    if url.startswith('http://www.'):
+        user_url = url[len('http://www.'):]
+        return user_url
+    else:
+        return 'Invalid'
 
 print(url_check('http://www.raghib.com'))
