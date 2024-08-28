70714f1af47f7edd4616b571ce5bdb013b28a3d2
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-08-28 08:24:09 +0500

    Update Q1.py

--- Lab 2/Q1.py
+++ Lab 2/Q1.py
@@ -1,8 +1,25 @@
-def calculate_area(length,width):
-    area = length * width
+def areaTrap(a,b,h):
+    area = ((a + b)/2) * h
     return area
 
-length = int(input('Enter the length: '))
-width= int(input('Enter the width: '))
+def areaParallel(b,h):
+    area = b * h
+    return area
+    
+def areaCyl(r,h):
+    area  = 2*(3.14)*r*h + 2 *3.14*r*r
+    return area
+    
+def volCyl(r,h):
+    area = 3.14*r*r*h
+    return area
 
-print(calculate_area(length,width))
+a = int(input('Enter the value for trapezoid: '))
+b = int(input('Enter the value for base: '))
+r = int(input('Enter the radius: '))
+h = int(input('Enter the height: '))
+
+print(areaTrap(a,b,h))
+print(areaParallel(b,h))
+print(areaCyl(r,h))
+print(volCyl(r,h))
