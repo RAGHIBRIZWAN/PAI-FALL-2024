7f7349a1486ad8224561ddf9bb69e06c61811899
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-09-04 09:40:23 +0500

    Update Task6.py

--- Lab 3/Task6.py
+++ Lab 3/Task6.py
@@ -1,9 +1,16 @@
-sentence = str(input('Enter the sentence: '))
-
-for i in range(len(sentence)):
-    if sentence[i] == '?':
-        try:
+def ask(sentence):
+    try:
+        if sentence[len(sentence)-1] == '?':
             with open('Questions.txt', 'w') as fileObj:
                 fileObj.write(sentence)
-        except Exception as e:
-            print(str(e))
+                print('It is a question, Accepted!')
+        else:
+            print('Not a question!')
+    except IOError as e:
+        print(str(e))
+    except Exception as a:
+        print(str(a))
+
+sentence = str(input('Enter the sentence: '))
+
+ask(sentence)
