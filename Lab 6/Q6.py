0fe95f504612c99829dfa3e361689facda825891
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-09-28 19:18:13 +0500

    Update Q6.py

--- Lab 6/Q6.py
+++ Lab 6/Q6.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('alcohol_consumption.csv')
 
-filtered_df = df[(df['Year'] == 1987) | (df['Year'] == 1989)]
+filtered = df[(df['Year'] == 1987) | (df['Year'] == 1989)]
 
-print(filtered_df)
+print(filtered)
