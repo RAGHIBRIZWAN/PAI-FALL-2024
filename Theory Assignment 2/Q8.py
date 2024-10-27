2ec71d07caa09b5027725b8b14dee865ca1b0aa5
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-10-27 20:13:58 +0500

    Update Q8.py

--- Theory Assignment 2/Q8.py
+++ Theory Assignment 2/Q8.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-A = np.array([[2, -1, 1],
+a = np.array([[2, -1, 1],
               [1, 3, 2],
               [1, -1, 2]])
 
-B = np.array([8, 13, 17])
+b = np.array([8, 13, 17])
 
-solution = np.linalg.solve(A, B)
+solution = np.linalg.solve(a, b)
 
 print("Solution:", solution)
