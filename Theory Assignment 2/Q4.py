3c1e29929126a2be04c1e82bc3f04f4bceb30c53
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2024-10-27 20:10:16 +0500

    Create Q4.py

---
+++ Theory Assignment 2/Q4.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.arange(1,6)
+b = np.arange(6,11)
+
+sum = np.add(a,b)
+sub = np.subtract(a,b)
+mul = np.multiply(a,b)
+div = np.divide(a,b)
+
+print('Sum:',sum)
+print('Subtract:',sub)
+print('Multiply:',mul)
+print('Divide:',div)
